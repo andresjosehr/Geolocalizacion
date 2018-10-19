9df6ff2ad42a9c0cd8467f0c2cd28ec48da9e775
Jose Hernandez <andres.hernandez@techlatam.la>
2018-10-19 14:02:29 -0400

    Commit Inicial

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,205 @@
+{{-- @extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Login') }}</div>
+
+                <div class="card-body">
+                    <form method="POST" action="{{ route('login') }}">
+                        @csrf
+
+                        <div class="form-group row">
+                            <label for="email" class="col-sm-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
+
+                                @if ($errors->has('email'))
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $errors->first('email') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
+
+                                @if ($errors->has('password'))
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $errors->first('password') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <div class="col-md-6 offset-md-4">
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+
+                                    <label class="form-check-label" for="remember">
+                                        {{ __('Remember Me') }}
+                                    </label>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="form-group row mb-0">
+                            <div class="col-md-8 offset-md-4">
+                                <button type="submit" class="btn btn-primary">
+                                    {{ __('Login') }}
+                                </button>
+
+                                <a class="btn btn-link" href="{{ route('password.request') }}">
+                                    {{ __('Forgot Your Password?') }}
+                                </a>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection --}}
+
+
+
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <!-- Meta, title, CSS, favicons, etc. -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Geolocalizacion</title>
+
+    <!-- Bootstrap -->
+    <link href="bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Font Awesome -->
+    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet">
+    <!-- NProgress -->
+    <link href="nprogress/nprogress.css" rel="stylesheet">
+    <!-- Animate.css -->
+    <link href="animate.css/animate.min.css" rel="stylesheet">
+
+    <!-- Custom Theme Style -->
+    <link href="css/custom.min.css" rel="stylesheet">
+  </head>
+
+  <body class="login">
+    <div>
+      <a class="hiddenanchor" id="signup"></a>
+      <a class="hiddenanchor" id="signin"></a>
+
+      <div class="login_wrapper">
+        <div class="animate form login_form">
+          <section class="login_content">
+            <form method="POST" action="{{ route('login') }}">
+                {{ csrf_field() }}
+              <h1>Panel de Ingreso</h1>
+              <h2 style="color: red;">{{ $errors->first('credenciales'), '<span class="help-block"></span>' }}</h2>
+              <div>
+                <input type="email" name="email" id="email" class="form-control {{ $errors->has('email') ? ' is-invalid' : '' }}" placeholder="E-mail" value="{{ old('email')}}" required />
+                {{ $errors->first('email'), '<span class="help-block">Email Invalido</span>' }}
+              </div>
+                @if ($errors->has('email'))
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $errors->first('email') }}</strong>
+                    </span>
+                @endif
+              <div>
+                <input type="password" id="password" name="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" placeholder="Contraseña" required />
+                {{ $errors->first('password'), '<span class="help-block">Contraseña Invalida</span>' }}  
+              </div>
+              @if ($errors->has('password'))
+                  <span class="invalid-feedback" role="alert">
+                      <strong>{{ $errors->first('password') }}</strong>
+                  </span>
+              @endif
+              <div class="col-md-6 offset-md-4">
+                  <div class="form-check">
+                      <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+                      <label class="form-check-label" for="remember">
+                          {{ __('Remember Me') }}
+                      </label>
+                  </div>
+              </div>
+              <div>
+                <button class="btn btn-default submit btn-block btn-primary" type="submit">Ingresar</button><br>
+                <a class="reset_pass" href="#">Lost your password?</a>
+              </div>
+
+              <div class="clearfix"></div>
+
+              <div class="separator">
+                <p class="change_link">New to site?
+                  <a href="#signup" class="to_register"> Create Account </a>
+                </p>
+
+                <div class="clearfix"></div>
+                <br />
+
+                <div>
+                  <h1><i class="fa fa-paw"></i> Gentelella Alela!</h1>
+                  <p>©2016 All Rights Reserved. Gentelella Alela! is a Bootstrap 3 template. Privacy and Terms</p>
+                </div>
+              </div>
+            </form>
+          </section>
+        </div>
+
+        <div id="register" class="animate form registration_form">
+          <section class="login_content">
+            <form>
+              <h1>Create Account</h1>
+              <div>
+                <input type="text" class="form-control" placeholder="Username" required="" />
+              </div>
+              <div>
+                <input type="email" class="form-control" placeholder="Email" required="" />
+              </div>
+              <div>
+                <input type="password" class="form-control" placeholder="Password" required="" />
+              </div>
+              <div>
+                <a class="btn btn-default submit" href="index.html">Submit</a>
+              </div>
+
+              <div class="clearfix"></div>
+
+              <div class="separator">
+                <p class="change_link">Already a member ?
+                  <a href="#signin" class="to_register"> Log in </a>
+                </p>
+
+                <div class="clearfix"></div>
+                <br />
+
+                <div>
+                  <h1><i class="fa fa-paw"></i> Gentelella Alela!</h1>
+                  <p>©2016 All Rights Reserved. Gentelella Alela! is a Bootstrap 3 template. Privacy and Terms</p>
+                </div>
+              </div>
+            </form>
+          </section>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
+
---
+++ resources/views/home.blade.php
@@ -0,0 +1,160 @@
+{{-- @extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Dashboard</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    You are logged in!
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
+ --}}
+
+
+             
+
+            @include("header");
+
+    <style>
+      .map {
+        height: 120%;
+        width: 100%;
+      }
+      body .section-mapa{
+        padding: 0px !important;
+        min-height: 100% !important;
+        max-height: 100% !important;
+      }
+    </style>
+
+
+        <!-- page content -->
+        <div class="right_col section-mapa" role="main">
+           <div id="map" class="map"></div>
+        </div>
+        <div id="popup">
+        </div>
+        <!-- /page content -->
+
+
+      </div>
+    </div>
+
+    <script type="text/javascript">
+
+
+(function(){
+    var vectorSource = new ol.source.Vector({
+      //create empty vector
+    });
+    
+    //create a bunch of icons and add to source vector
+ 
+        var iconFeature = new ol.Feature({
+          geometry: new  
+          ol.geom.Point([-72.069960, -37.157991]),
+          name: 'Null Island',
+          population: 4000,
+          rainfall: 500
+          });
+          vectorSource.addFeature(iconFeature);
+
+
+
+
+
+
+    //create the style
+    var iconStyle = new ol.style.Style({
+      image: new ol.style.Icon(/** @type {olx.style.IconOptions} */ ({
+        anchor: [0.5, 46],
+        anchorXUnits: 'fraction',
+        anchorYUnits: 'pixels',
+        opacity: 0.75,
+        src: 'http://openlayers.org/en/v3.9.0/examples/data/icon.png'
+      }))
+    });
+
+
+
+    //add the feature vector to the layer vector, and apply a style to whole layer
+    var vectorLayer = new ol.layer.Vector({
+      source: vectorSource,
+      style: iconStyle
+    });
+
+    var map = new ol.Map({
+      layers: [new ol.layer.Tile({ source: new ol.source.OSM() }), vectorLayer],
+      target: document.getElementById('map'),
+      view: new ol.View({
+      center: [-72.069960, -37.157991],
+      zoom: 10,
+      projection: 'EPSG:4326'
+      })
+    });
+
+
+
+
+})();
+
+
+
+    </script>
+
+    <script src="https://cdn.jsdelivr.net/bootstrap.tagsinput/0.4.2/bootstrap-tagsinput.min.js"></script>
+
+    <!-- Bootstrap -->
+    <script src="bootstrap/dist/js/bootstrap.min.js"></script>
+    <!-- FastClick -->
+    <script src="fastclick/lib/fastclick.js"></script>
+    <!-- NProgress -->
+    <script src="nprogress/nprogress.js"></script>
+    <!-- Chart.js -->
+    <script src="Chart.js/dist/Chart.min.js"></script>
+    <!-- gauge.js -->
+    <script src="gauge.js/dist/gauge.min.js"></script>
+    <!-- bootstrap-progressbar -->
+    <script src="bootstrap-progressbar/bootstrap-progressbar.min.js"></script>
+    <!-- iCheck -->
+    <script src="iCheck/icheck.min.js"></script>
+    <!-- Skycons -->
+    <script src="skycons/skycons.js"></script>
+    <!-- Flot -->
+    <script src="Flot/jquery.flot.js"></script>
+    <script src="Flot/jquery.flot.pie.js"></script>
+    <script src="Flot/jquery.flot.time.js"></script>
+    <script src="Flot/jquery.flot.stack.js"></script>
+    <script src="Flot/jquery.flot.resize.js"></script>
+    <!-- Flot plugins -->
+    <script src="flot.orderbars/js/jquery.flot.orderBars.js"></script>
+    <script src="flot-spline/js/jquery.flot.spline.min.js"></script>
+    <script src="flot.curvedlines/curvedLines.js"></script>
+    <!-- DateJS -->
+    <script src="DateJS/build/date.js"></script>
+    <!-- JQVMap -->
+    <script src="jqvmap/dist/jquery.vmap.js"></script>
+    <script src="jqvmap/dist/maps/jquery.vmap.world.js"></script>
+    <script src="jqvmap/examples/js/jquery.vmap.sampledata.js"></script>
+    <!-- bootstrap-daterangepicker -->
+    <script src="moment/min/moment.min.js"></script>
+    <script src="bootstrap-daterangepicker/daterangepicker.js"></script>
+
+    <!-- Custom Theme Scripts -->
+    <script src="js/custom.min.js"></script>
+    
+  </body>
+</html>
